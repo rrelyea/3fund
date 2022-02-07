c117d2626d93d9050560e48a25c2fd4766658d0b
Rob Relyea <rob@relyeas.net>
2022-02-06 19:29:25 -0800

    monthly and daily data

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     download: true,
     complete: function(data) {
       monthlyStock = data;
-      renderPage(monthlyStock, monthlyIntlStock, monthlyBond, dailyStock, dailyIntlStock, dailyBond);
     }
   });
 
@@ -21,7 +20,6 @@ function App() {
     download: true,
     complete: function(data) {
       monthlyIntlStock = data;
-      renderPage(monthlyStock, monthlyIntlStock, monthlyBond, dailyStock, dailyIntlStock, dailyBond);
     }
   });
 
@@ -29,7 +27,6 @@ function App() {
     download: true,
     complete: function(data) {
       monthlyBond = data;
-      renderPage(monthlyStock, monthlyIntlStock, monthlyBond, dailyStock, dailyIntlStock, dailyBond);
     }
   });
 
@@ -37,7 +34,6 @@ function App() {
     download: true,
     complete: function(data) {
       dailyStock = data;
-      renderPage(dailyStock, dailyIntlStock, dailyBond, dailyStock, dailyIntlStock, dailyBond);
     }
   });
 
@@ -45,7 +41,6 @@ function App() {
     download: true,
     complete: function(data) {
       dailyIntlStock = data;
-      renderPage(dailyStock, dailyIntlStock, dailyBond, dailyStock, dailyIntlStock, dailyBond);
     }
   });
 
@@ -53,7 +48,7 @@ function App() {
     download: true,
     complete: function(data) {
       dailyBond = data;
-      renderPage(dailyStock, dailyIntlStock, dailyBond, dailyStock, dailyIntlStock, dailyBond);
+      renderPage();
     }
   });
 
@@ -63,49 +58,112 @@ function App() {
         <p id='status'>
           Loading price data
         </p>
-        <div id='dates'>
-
-        </div>
-        <div id='stock'>
-
-        </div>
-        <div id='stockIntl'>
-
-        </div>
-        <div id='bond'>
-
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Dates</th>
+              <th>Stocks</th>
+              <th>IntlStocks</th>
+              <th>Bonds</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><div id='dates'>
+              </div></td>
+              <td><div id='stock'>
+              </div></td>
+              <td><div id='stockIntl'>
+              </div></td>
+              <td><div id='bond'>
+              </div></td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
 
-var rendered = false;
+var count = 0;
 
 function renderPage() {
-
+  console.log("renderpage");
   var status = document.getElementById('status');
   var dates = document.getElementById('dates');
   var stock = document.getElementById('stock');
   var stockIntl = document.getElementById('stockIntl');
   var bond = document.getElementById('bond');
-  if (monthlyStock != null && monthlyIntlStock != null && monthlyBond != null && dailyStock != null && dailyIntlStock != null && dailyBond != null && rendered === false)
+  if (monthlyStock != null && monthlyIntlStock != null && monthlyBond != null && dailyStock != null && dailyIntlStock != null && dailyBond != null && count === 1)
   {
-    rendered = true;
+    console.log("dataready");
     dates.innerText = "";
     stock.innerText = "VTI";
     stockIntl.innerText = "VXUS";
     bond.innerText = "BND";
     status.textContent = "Daily prices";
-    var length = dailyStock.data.length;
 
-    for (var i = 1; i < length - 1; i++) {
-        dates.innerText += "  " + dailyStock.data[i][0];
-        stock.innerText += "  " + dailyStock.data[i][1];
-        stockIntl.innerText += "  " + dailyIntlStock.data[i][1];
-        bond.innerText += "  " + dailyBond.data[i][1];
+
+    var monthlyLength = Math.max(monthlyStock.data.length, monthlyIntlStock.data.length, monthlyBond.data.length); 
+    console.log(monthlyLength);
+
+    var iStocks = 1;
+    var iStocksIntl = 1;
+    var iBonds = 1;
+    while (true) {
+      if (monthlyStock.data[iStocks].length === 1 
+        || monthlyIntlStock.data[iStocksIntl].length === 1 
+        || monthlyBond.data[iBonds].length === 1) {
+          return;
+      }
+      else
+      {
+        var s = monthlyStock.data[iStocks][0];
+        var sI = monthlyIntlStock.data[iStocksIntl][0];
+        var b = monthlyBond.data[iBonds][0];
+        console.log(s, sI, b);
+
+        var max;
+        if (s < sI) {
+          max = sI;
+        } else {
+          max = s;
+        }
+        if (max < b)
+        {
+          max = b;
+        }
+        
+        if (s < max) iStocks++;
+        if (sI < max) iStocksIntl++;
+        if (b < max) iBonds++;
+
+        if (s === sI && sI === b)
+        {
+          console.log(iStocks, iStocksIntl, iBonds);
+          dates.innerText += "  " + monthlyStock.data[iStocks][0];
+          stock.innerText += "  " + monthlyStock.data[iStocks][1];
+          stockIntl.innerText += "  " + monthlyIntlStock.data[iStocksIntl][1];
+          bond.innerText += "  " + monthlyBond.data[iBonds][1];
+          iStocks++;
+          iStocksIntl++;
+          iBonds++;
+        }
+      }
     }
+    
+    // var dailyLength = dailyStock.data.length;
+    // for (var i = 1; i < dailyLength - 1; i++) {
+    //     dates.innerText += "  " + dailyStock.data[i][0];
+    //     stock.innerText += "  " + dailyStock.data[i][1];
+    //     stockIntl.innerText += "  " + dailyIntlStock.data[i][1];
+    //     bond.innerText += "  " + dailyBond.data[i][1];
+    // }
+
+    }
+
+    count = count + 1;
   }
-}
+
 
 export default App;
