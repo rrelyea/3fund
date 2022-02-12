4a341075aff80b425cd337d0fec0bb3df30e3a8e
Rob Relyea <rob@relyeas.net>
2022-02-12 12:03:43 -0800

    allow picking fund family. use querystring for aa and fundfamily.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ class App extends React.Component {
   state = {
     monthlyQuotes: null,
     dailyQuotes: null,
-    indexes: [1,1,1],
     startYear: 0,
     startMonth: 0,
     statusMessage: "Loading prices",
-    tickers: ['VTI', 'VXUS', 'BND'],
-    allocations: [2/3, .2, 1/3]
   };
 
   async toCsv(uri) {
@@ -21,78 +18,143 @@ class App extends React.Component {
         complete (results, file) {
           resolve(results.data)
         },
-        error (err, file) {
-          reject(err)
+        error (error, file) {
+          resolve(null)
         }
       })
     })
   }
 
-  async componentDidMount () {
-    this.state.monthlyQuotePromises = [
+  setTickers ()
+  {
+    this.state.tickers = null;
+    var eof = false;
+    var i = 1;
+    while (!eof) {
+      if (this.state.fundTypes[i][0] !== '') {
+        if (this.state.fundTypes[i][0].toUpperCase() == this.state.type.toUpperCase()) {
+          this.state.tickers = [this.state.fundTypes[i][1], this.state.fundTypes[i][2], this.state.fundTypes[i][3]];
+        }
+
+        i++;
+      } else {
+        eof = true;
+      }
+    }
+  }
+
+  async loadFundInfo() {
+    this.state.monthlyQuotes = [null, null, null];
+    this.state.dailyQuotes = [null, null, null];
+
+    this.setTickers();
+
+    if (this.state.tickers === null) return null;
+
+    var monthlyQuotePromises = [
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/M_" + this.state.tickers[0] + ".csv"),
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/M_" + this.state.tickers[1] + ".csv"),
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/M_" + this.state.tickers[2] + ".csv")];
-    this.state.dailyQuotePromises = [
+    var dailyQuotePromises = [
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/D_" + this.state.tickers[0] + ".csv"),
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/D_" + this.state.tickers[1] + ".csv"),
       this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/D_" + this.state.tickers[2] + ".csv")];
 
-    this.state.monthlyQuotes = await Promise.all(this.state.monthlyQuotePromises);
-    this.state.dailyQuotes = await Promise.all(this.state.dailyQuotePromises);
+    this.state.monthlyQuotes = await Promise.all(monthlyQuotePromises);
+    this.state.dailyQuotes = await Promise.all(dailyQuotePromises);
 
-    this.state.dailyQuotePromises = null;
-    this.state.monthlyQuotePromises = null;
-
-    var loop = true;
-    while (loop) {
-      if (this.state.monthlyQuotes[0][this.state.indexes[0]].length === 1 
-        || this.state.monthlyQuotes[1][this.state.indexes[1]].length === 1 
-        || this.state.monthlyQuotes[2][this.state.indexes[2]].length === 1) {
-          loop = false;
-      } else {
-        var s = this.state.monthlyQuotes[0][this.state.indexes[0]][0];
-        var sI = this.state.monthlyQuotes[1][this.state.indexes[1]][0];
-        var b = this.state.monthlyQuotes[2][this.state.indexes[2]][0];
-  
-        var max;
-        if (s < sI) {
-          max = sI;
+    this.state.indexes = [1,1,1];
+    if (this.state.monthlyQuotes[0] !== null) {
+      var loop = true;
+      while (loop) {
+        if (this.state.monthlyQuotes[0][this.state.indexes[0]].length === 1 
+          || this.state.monthlyQuotes[1][this.state.indexes[1]].length === 1 
+          || this.state.monthlyQuotes[2][this.state.indexes[2]].length === 1) {
+            loop = false;
         } else {
-          max = s;
-        }
-        if (max < b) {
-          max = b;
-        }
-        
-        if (s < max) this.state.indexes[0] = this.state.indexes[0] + 1;
-        if (sI < max) this.state.indexes[1] = this.state.indexes[1] + 1;
-        if (b < max) this.state.indexes[2] = this.state.indexes[2] + 2;
-  
-        if (s === sI && sI === b) {
-          var date = this.state.monthlyQuotes[0][this.state.indexes[0]][0];
-          this.state.startYear = parseInt(date.substr(0,4));
-          this.state.startMonth = parseInt(date.substr(5,2));
-          loop = false;
+          var s = this.state.monthlyQuotes[0][this.state.indexes[0]][0];
+          var sI = this.state.monthlyQuotes[1][this.state.indexes[1]][0];
+          var b = this.state.monthlyQuotes[2][this.state.indexes[2]][0];
+    
+          var max;
+          if (s < sI) {
+            max = sI;
+          } else {
+            max = s;
+          }
+          if (max < b) {
+            max = b;
+          }
+          
+          if (s < max) this.state.indexes[0] = this.state.indexes[0] + 1;
+          if (sI < max) this.state.indexes[1] = this.state.indexes[1] + 1;
+          if (b < max) this.state.indexes[2] = this.state.indexes[2] + 2;
+    
+          if (s === sI && sI === b) {
+            var date = this.state.monthlyQuotes[0][this.state.indexes[0]][0];
+            this.state.startYear = parseInt(date.substr(0,4));
+            this.state.startMonth = parseInt(date.substr(5,2));
+            loop = false;
+          }
         }
       }
+      this.state.statusMessage = "3fund Returns";
+    } else {
+      this.state.statusMessage = "No data found";
     }
-    this.state.statusMessage = "3fund Returns";
-    this.setState(this.state.monthlyQuotes);
+
+    this.setState(this.state);
+  }
+
+  async componentDidMount () {
+    const params = new URLSearchParams(window.location.search);
+
+    this.state.type = params.has('type') ? params.get('type') : 'VanguardETF';
+    var fundTypesPromise = this.toCsv("https://raw.githubusercontent.com/rrelyea/3fund-prices/main/data/fundTypes.csv");
+    this.state.fundTypes = await fundTypesPromise;
+
+    this.state.aa = params.has('aa') ? params.get('aa') : '70/30';
+    this.state.intl = params.has('intl') ? params.get('intl') : '0s';
+    var sbAllocations = this.state.aa.split('/');
+    var sAllocation = parseInt(sbAllocations[0]) / 100;
+    var bAllocation = parseInt(sbAllocations[1]) / 100;
+    var iAllocation = this.state.intl !== null ? parseInt(this.state.intl) / 100 : 0;
+    this.state.allocations = [sAllocation,iAllocation,bAllocation];
+
+    this.loadFundInfo();
   }
 
   render () {
+    const handleChange = (e) => {
+      this.state.type = e.target.value;
+      const params = new URLSearchParams(window.location.search);
+      params.set('type', this.state.type);
+      window.history.replaceState({}, "", `${window.location.pathname}?${params.toString()}`);
+      this.loadFundInfo();
+      this.render();
+    }
+
     return  (this.state.monthlyQuotes !== null) && 
       <div>
         <header className="App-header">
           <h3 id='status'>
             {this.state.statusMessage}
           </h3>
+          <div>
+            <label htmlFor='fundType'>Fund Family:&nbsp;</label>
+            <select id='fundType' value={this.state.type} onChange={(e) => handleChange(e)}>
+              {this.state.fundTypes.map((fundType,index) => 
+                <option key={index} value={fundType.length > 0 ? fundType[0] : "filler"}>{fundType.length > 0 ? fundType[0] : "filler"}</option>
+              )} 
+            </select>
+          </div>
           <div id='tickers'>
             {this.state.tickers.join(" - ")}
           </div>
           <div id='allocations'>
+            <span>
             {this.state.allocations.map(allocation => <span>{(allocation * 100).toFixed(1)+"%  "}</span>  )}
+            </span>
           </div>
           <table>
             <tr>
@@ -185,6 +247,8 @@ function getDayChange(data, yearA, monthA, dayIndex, fund) {
 }
 
 function showYears (data) {
+  if (data.monthlyQuotes[0] === null) return null;
+
   var assetStock = data.allocations[0];
   var assetStockIntl = data.allocations[1];
   var assetBond = data.allocations[2];
@@ -211,11 +275,15 @@ function showYears (data) {
   return years.map( period => <tr><td>{period[0]}</td><td className='value'>{Number(period[1]).toFixed(2)+"%"}</td></tr> );
 }
 
+var month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 function showMonths (data) {
+  if (data.monthlyQuotes[0] === null) return null;
+
   var assetStock = data.allocations[0];
   var assetStockIntl = data.allocations[1];
   var assetBond = data.allocations[2];
-  var currentMonth = new Date(). getMonth() + 1;
+  var currentMonth = new Date().getMonth() + 1;
   var currentYear = new Date().getFullYear();
   var months = Array(currentMonth);
   
@@ -228,7 +296,7 @@ function showMonths (data) {
       (assetBond * 100 * delta3);
     composite = delta1 == null || delta2 == null || delta3 == null ? null : composite;
     months[currentMonth - month] = new Array(2);
-    months[currentMonth - month][0] = month < 10 ? "0" + month.toString() : month.toString();
+    months[currentMonth - month][0] = month_names_short[month-1];
     months[currentMonth - month][1] = composite;
   }
 
@@ -236,6 +304,8 @@ function showMonths (data) {
 }
 
 function showDays (data) {
+  if (data.monthlyQuotes[0] === null) return null;
+
   var assetStock = data.allocations[0];
   var assetStockIntl = data.allocations[1];
   var assetBond = data.allocations[2];
